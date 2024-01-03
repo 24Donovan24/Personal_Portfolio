fab0a081783cbf1d705294d262180e158a4e0529
donovan <donovanlee_25@hotmail.com>
2024-01-03 23:33:22 +0800

    Add hover

--- src/components/Sections/Resume/Skills.tsx
+++ src/components/Sections/Resume/Skills.tsx
@@ -1,4 +1,6 @@
-import { FC } from 'react';
+/* eslint-disable react-memo/require-memo */
+import {FC} from 'react';
+
 import {LogoWithText} from '../../../data/dataDef';
 
 // export const SkillGroup: FC<PropsWithChildren<{ skillGroup: SkillGroupType }>> = memo(({ skillGroup }) => {
@@ -31,11 +33,13 @@ import {LogoWithText} from '../../../data/dataDef';
 //   );
 // };
 
-const LogoWithText: FC<LogoWithText> = ({ src, alt, text }) => (
-    <div className='flex flex-col items-center mx-4 my-6'>
-    <img src={src} className='w-16 h-16 my-2 object-contain' alt={alt}></img>
-    <p className="text-center">{text}</p>
-  </div>
+const LogoWithText: FC<LogoWithText> = ({src, alt, text}) => (
+    <div className='flex flex-col items-center mx-4 my-6 relative group'>
+    <img alt={alt} className='w-16 h-16 my-2 object-contain' src={src}></img>
+      <p className="text-center absolute bg-gray-800 text-white p-2 px-4 py-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100 rounded-lg">
+        {text}
+      </p>
+    </div>
 );
 
 export const SkillGroup = () => {
@@ -43,36 +47,36 @@ export const SkillGroup = () => {
     <div className='flex flex-col'>
       <h2 className="text-xl font-bold">Languages</h2>
         <div className='flex flex-row'>
-          <LogoWithText src='/java.png' alt="Java" text="Java" />
-          <LogoWithText src='/python.png' alt="Python" text="Python" />
-          <LogoWithText src='/javascript.png' alt="JavaScript" text="JavaScript" />
-          <LogoWithText src='/c.png' alt="C" text="C" />
-          <LogoWithText src='/csharp.png' alt="C#" text="C#" />
-          <LogoWithText src='/html.png' alt="HTML" text="HTML" />
-          <LogoWithText src='/css.png' alt="CSS" text="CSS" />
-          <LogoWithText src='/sql.png' alt="SQL" text="SQL" />
+          <LogoWithText alt="Java" src='/java.png' text="Java" />
+          <LogoWithText alt="Python" src='/python.png' text="Python" />
+          <LogoWithText alt="JavaScript" src='/javascript.png' text="JavaScript" />
+          <LogoWithText alt="C" src='/c.png' text="C" />
+          <LogoWithText alt="C#" src='/csharp.png' text="C#" />
+          <LogoWithText alt="HTML" src='/html.png' text="HTML" />
+          <LogoWithText alt="CSS" src='/css.png' text="CSS" />
+          <LogoWithText alt="SQL" src='/sql.png' text="SQL" />
         </div>
 
       <h2 className="text-xl font-bold">Libraries and Frameworks</h2>
         <div className='flex flex-row'>
-          <LogoWithText src='/react.png' alt="React" text="React" />
-          <LogoWithText src='/angular.png' alt="Angular" text="Angular" />
-          <LogoWithText src='/node.png' alt="Node.js" text="Node.js" />
+          <LogoWithText alt="React" src='/react.png' text="React" />
+          <LogoWithText alt="Angular" src='/angular.png' text="Angular" />
+          <LogoWithText alt="Node.js" src='/node.png' text="Node.js" />
           {/* <LogoWithText src='/express.png' alt="Express" text="Express" /> */}
-          <LogoWithText src='/net.png' alt=".NET Core" text=".NET Core" />
+          <LogoWithText alt=".NET Core" src='/net.png' text=".NET Core" />
         </div>
 
       <h2 className="text-xl font-bold">Databases</h2>
         <div className='flex flex-row'>
-          <LogoWithText src='/microsoftsql.png' alt="Microsoft SQL" text="Microsoft SQL" />
-          <LogoWithText src='/postgresql.png' alt="PostgreSQL" text="PostgreSQL" />
+          <LogoWithText alt="Microsoft SQL" src='/microsoftsql.png' text="Microsoft SQL" />
+          <LogoWithText alt="PostgreSQL" src='/postgresql.png' text="PostgreSQL" />
           {/* <LogoWithText src='/mysql.png' alt="MySQL" text="MySQL" /> */}
         </div>
 
       <h2 className="text-xl font-bold">DevOps</h2>
         <div className='flex flex-row'>
-          <LogoWithText src='/azure.png' alt="Azure DevOps" text="Azure DevOps" />
-          <LogoWithText src='/docker.png' alt="Docker" text="Docker" />
+          <LogoWithText alt="Azure DevOps" src='/azure.png' text="Azure DevOps" />
+          <LogoWithText alt="Docker" src='/docker.png' text="Docker" />
         </div>
     </div>
   );
